95a488c20837658f19eab1b7bd72d8155c25400a
Rodion Bykov <rodionbykov@gmail.com>
2016-01-14 17:39:54 +0100

    Search added to controller

--- maven-4-spring-mvc-jpa-hibernate/src/main/java/com/rodionbykov/userdirectory/controller/HomeController.java
+++ maven-4-spring-mvc-jpa-hibernate/src/main/java/com/rodionbykov/userdirectory/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -54,10 +55,10 @@ public class HomeController {
     }
 	
 	@RequestMapping(value="/search", method=RequestMethod.GET)
-    public ModelAndView userSearchPage() {
+    public ModelAndView userSearchPage(@RequestParam(value = "searchstring", required = false) String s) {
         ModelAndView mav = new ModelAndView("list");
         
-        List<User> userList = userService.search("luke");
+        List<User> userList = userService.search(s);
         
         mav.addObject("userList", userList);
         
